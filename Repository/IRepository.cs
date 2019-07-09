fbdd651f797732793bef62b72b188499ad96b279
Ninisbeth Segovia Noguera <ninisbeth_segovia@hotmail.com>
2019-07-09 15:30:57 -0500

    Pruebas MVC con Repositorio para Entity Framework

---
+++ Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IRepository : IDisposable
+    {
+        TEntity Create<TEntity>(TEntity newEntity) where TEntity : class;
+        bool Update<TEntity>(TEntity modifiedEntity) where TEntity : class;
+        bool Delete<TEntity>(TEntity deletedEntity) where TEntity : class;
+        TEntity FindEntity<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+        IEnumerable<TEntity> FindEntitySet<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+
+    }
+}
